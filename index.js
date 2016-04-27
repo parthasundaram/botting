82b836a7a0d00a15255cb78ae957ac07bec2ef95
Partha Sundaram <Partha@Parthas-MacBook-Air.local>
2016-04-26 20:47:07 -0700

    postback handling

--- index.js
+++ index.js
@@ -99,12 +99,7 @@ function sendGenericMessage(sender) {
                         "type": "postback",
                         "title": "Postback",
                         "payload": "Payload for second element in a generic bubble",
-                    },
-                    {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
+                    }, {
                         "type": "postback",
                         "title": "Postback",
                         "payload": "Payload for second element in a generic bubble",
